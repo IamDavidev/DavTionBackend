522f6805e53be304898bdb9f6aeab3152b95d030
itsDavidev <davidlez2mgk@gmail.com>
2022-05-12 18:45:30 -0400

    🚀 config server

--- index.js
+++ index.js
@@ -2,8 +2,37 @@ import { ApolloServer, gql } from 'apollo-server-express'
 import express from 'express'
 import http from 'http'
 
-const typeDefs = gql``
-const resolvers = {}
+const typeDefs = gql`
+    type Task {
+        id: ID
+        title: String
+        createdAt: String
+        finishedAt: String
+        description: String
+        status: String
+        priority: String
+    }
+    type Query {
+        tasks: [Task]
+    }
+`
+const resolvers = {
+    Query: {
+        tasks: () => {
+            return [
+                {
+                    id: '1',
+                    title: 'Task 1',
+                    createdAt: '2020-01-01',
+                    finishedAt: '2020-01-01',
+                    description: 'Description 1',
+                    status: 'Status 1',
+                    priority: 'Priority 1'
+                }
+            ]
+        }
+    }
+}
 const PORT = process.env.PORTSERVER || 3001
 
 async function MainServer(typeDefs, resolvers) {
@@ -14,17 +43,14 @@ async function MainServer(typeDefs, resolvers) {
         typeDefs,
         resolvers,
         csrfPrevention: true
-
     })
 
     await server.start();
-    server.applyMiddleware({ app })
-
-    await new Promise(resolve => {
-        httpServer.listen({
-            port: PORT
-        }), resolve
+    server.applyMiddleware({
+        app,
+        path: '/'
     })
+    await new Promise(resolve => httpServer.listen({ port: PORT }, resolve))
     console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`)
 }
 
